73b8271f5d7d89a274b50b9bf1ece06882f19f23
danjelous <daniel.brunner@live.at>
2018-05-29 16:11:19 +0200

    Add slack hook

--- app/index.mjs
+++ app/index.mjs
@@ -1,13 +1,29 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
 import express from 'express';
 import jsdom from 'jsdom';
 const { JSDOM } = jsdom;
 
 const app = express();
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
 app.get('/', (req, res) => {
   res.send('Up and running');
 });
+
+const postSlackMessage = msg => {
+  axios.post(process.env.SLACK_HOOK, {
+      'text': `${msg} :beers:`
+    })
+    .then((response) => {
+      console.log('Sent beer to slack!');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 const getBeer = (req, res) => {
   console.log('Getting beer...');
@@ -17,13 +33,14 @@ const getBeer = (req, res) => {
       console.log('Got beer!');
       const openingHours = dom.window.document.querySelector('#Geöffnet-oder-Geschlossen p').textContent;
       console.log(openingHours);
+      postSlackMessage(openingHours);
       res.send(openingHours);
     })
     .catch((error) => {
       console.log(error);
       res.send('Fuck this day, no bier, no gartl... </3');
     });
-}
+};
 
 app.get('/bier', getBeer);
 
